684b6e19305803eac9c1dea49696ff2bcf5aca89
Patrick Baessler <baessler@entrecode.de>
2022-08-21 12:37:50 +0200

    add better types

--- renderer/models/Instance.ts
+++ renderer/models/Instance.ts
@@ -4,12 +4,13 @@ import { v4 as uuid } from "uuid";
 export default class Instance {
   public id = uuid();
 
-  constructor(public type: string, public props: { [key: string]: any }) {}
+  constructor(public type: keyof p5, public props: { [key: string]: any }) {}
 
   run(p5: p5) {
     try {
       // try to execute the Instance as P5 function
       // and pass all props as arguments
+
       p5[this.type](...Object.values(this.props));
     } catch (e) {
       console.error(e, this.type, this.props);
