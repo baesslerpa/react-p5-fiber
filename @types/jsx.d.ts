ad620d83b7f7ca6fe83cacd8b5b4f18761e28665
Patrick Baessler <baessler@entrecode.de>
2022-08-21 12:37:58 +0200

    sugar

--- @types/jsx.d.ts
+++ @types/jsx.d.ts
@@ -1,35 +1,38 @@
+import "p5";
+
 import { ReactNode } from "react";
 
-type CustomElement<T> = Partial<T> & DOMAttributes<T> & { children: any };
+type CustomElement<T> = Partial<T>;
 
-export declare global {
-  export namespace JSX {
-    export interface IntrinsicElements {
-      // ["teyt"]: CustomElement<{Parameter<background>}>;
-      ["pixelDensity"]: CustomElement<{ scale: number }>;
-      /**
-       * Setup Function
-       * should only appear once
-       * @param {ReactNode} children
-       */
-      ["setup"]: CustomElement<{ children: ReactNode }>;
-      /** Draw function */
-      ["draw"]: CustomElement<{ children: ReactNode }>;
-      ["createCanvas"]: CustomElement<{ width: number; height: number }>;
-      ["background"]: CustomElement<{ color: number | string }>;
-      ["noStroke"]: CustomElement<{}>;
-      ["ellipseMode"]: CustomElement<{ mode: string }>;
-      ["ellipse"]: CustomElement<{
-        x: number;
-        y: number;
-        width: number;
-        height: number;
-      }>;
-      ["fill"]: CustomElement<{ color: number | string }>;
-      ["stroke"]: CustomElement<{ r: number; g: number; b: number; a: number }>;
-      ["beginShape"]: CustomElement<{}>;
-      ["vertex"]: CustomElement<{ x: number; y: number; key: number }>;
-      ["endShape"]: CustomElement<{}>;
-    }
+interface CustomJSX {
+  pixelDensity: CustomElement<{ scale: number }>;
+  /**
+   * Setup Function
+   * should only appear once
+   * @param {ReactNode} children
+   */
+  ["setup"]: CustomElement<{ children: ReactNode }>;
+  /** Draw function */
+  ["draw"]: CustomElement<{ children: ReactNode }>;
+  ["createCanvas"]: CustomElement<{ width: number; height: number }>;
+  ["background"]: CustomElement<{ color: number | string }>;
+  ["noStroke"]: CustomElement<{}>;
+  ["ellipseMode"]: CustomElement<{ mode: string }>;
+  ["ellipse"]: CustomElement<{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  }>;
+  ["fill"]: CustomElement<{ color: number | string }>;
+  ["stroke"]: CustomElement<{ r: number; g: number; b: number; a: number }>;
+  ["beginShape"]: CustomElement<{}>;
+  ["vertex"]: CustomElement<{ x: number; y: number; key: number }>;
+  ["endShape"]: CustomElement<{}>;
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements extends CustomJSX {}
   }
 }
